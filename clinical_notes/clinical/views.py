8e264062b53b34c23106cbc0beed22dc67419ec9
Luciana <luciana@drchrono.com>
2016-03-09 12:14:51 -0800

    Inital connection with drchono API.

---
+++ clinical_notes/clinical/views.py
@@ -0,0 +1,41 @@
+import sys
+from django.shortcuts import render, redirect
+sys.path.append("..")
+from SECRET import CLIENT_ID, CLIENT_SECRET
+
+import requests, datetime, pytz
+
+# Create your views here.
+
+def index(request):
+    if 'error' in request.GET:
+        raise ValueError('Error authorizing application: %s' % request.GET['error'])
+    
+    response = requests.post('https://drchrono.com/o/token/', data={
+        'code': request.GET['code'],
+        'grant_type': 'authorization_code',
+        'redirect_uri': 'https://127.0.0.1:8000/clinical/',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    })
+
+    response.raise_for_status()
+    data = response.json()
+
+    # Save these in your database associated with the user
+    request.session['access_token'] = data['access_token']
+    request.session['refresh_token'] = data['refresh_token']
+    request.session['expires_timestamp'] = str(datetime.datetime.now(pytz.utc) + datetime.timedelta(seconds=data['expires_in']))
+
+    context = {}
+    return redirect('home')
+
+def home(request):
+    headers = {
+        'Authorization': 'Bearer %s' % request.session['access_token'],
+    }
+
+    url = 'https://drchrono.com/api/....'
+
+    context = {}
+    return render(request, 'clinical.html', context)
--- clinical_notes/clinical_notes/views.py
+++ clinical_notes/clinical_notes/views.py
@@ -1,5 +1,12 @@
+import sys
 from django.shortcuts import render
+sys.path.append("..")
+from SECRET import REDIRECT_URI, CLIENT_ID, SCOPES
+from urllib import quote_plus
+
 
 def index(request):
-	context = {}
+	#URL = 'https://drchrono.com/o/authorize/?redirect_uri=%s&response_type=code&client_id=%s&scope=%s' % (REDIRECT_URI, quote_plus(CLIENT_ID) , quote_plus(SCOPES))
+	URL = 'https://drchrono.com/o/authorize/?redirect_uri=%s&response_type=code&client_id=%s' % (REDIRECT_URI, quote_plus(CLIENT_ID))
+	context = {"URL": URL}
 	return render(request, 'index.html', context)
